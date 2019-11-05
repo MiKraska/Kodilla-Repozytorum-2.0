2b89a8a25149240b80b8d97feacc2d85a17b9ef8
MiKraska <mikraska@gmail.com>
2019-11-05 15:12:41 +0100

    15.5 first commit

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/builder/bigmac/Bigmac.java
@@ -0,0 +1,98 @@
+package com.kodilla.patterns.builder.bigmac;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public final class Bigmac {
+
+    private static final Set<String> AVAILABLE_BUNS =
+            new HashSet<>(Arrays.asList("Sesame bun", "Standard bun"));
+    private static final Set<String> AVAILABLE_SAUCES =
+            new HashSet<>(Arrays.asList("Standard", "1000 Islands Dressing", "BBQ"));
+    private static final Set<String> AVAILABLE_INGREDIENTS =
+            new HashSet<>(Arrays.asList("Salad", "Onion", "Bacon", "Pickle", "Chili Pepper ", "Mushrooms", "Shrimps", "Cheese"));
+
+    private final int burgers;
+    private final String bun;
+    private final String sauce;
+    private final Set<String> ingredients;
+
+    public static class BigmacBuilder {
+
+        private String bun;
+        private int burgers;
+        private String sauce;
+        private Set<String> ingredients = new HashSet<>();
+
+        public BigmacBuilder bun(String bun) {
+            if (!AVAILABLE_BUNS.contains(bun)) {
+                throw new IllegalStateException("Wrong bun chosen. Available options are: Sesame bun, Standard bun.");
+            }
+            this.bun = bun;
+            return this;
+        }
+
+        public BigmacBuilder burgers(int burgers) {
+            if (burgers < 1 || burgers > 10) {
+                throw new IllegalStateException("You need to choose between 1 to 10 burgers.");
+            }
+            this.burgers = burgers;
+            return this;
+        }
+
+        public BigmacBuilder sauce(String sauce) {
+            if (!AVAILABLE_SAUCES.contains(sauce)) {
+                throw new IllegalStateException("Wrong sauce chosen. Available options are: Standard, 1000 Island Dressing, BBQ.");
+            }
+            this.sauce = sauce;
+            return this;
+        }
+
+        public BigmacBuilder ingredient(String ingredient) {
+            if (!AVAILABLE_INGREDIENTS.contains(ingredient)) {
+                throw new IllegalStateException("Wrong ingredient chosen. Available options are: Salad, Onion, Bacon," +
+                        "Pickle, Chili Peppers, Mushrooms, Shrimps, Cheese.");
+            }
+            this.ingredients.add(ingredient);
+            return this;
+        }
+
+        public Bigmac build() {
+            return new Bigmac(bun, burgers, sauce, ingredients);
+        }
+    }
+
+    private Bigmac(final String bun, final int burgers, final String sauce, final Set<String> ingredients) {
+        this.bun = bun;
+        this.burgers = burgers;
+        this.sauce = sauce;
+        this.ingredients = new HashSet<>(ingredients);
+    }
+
+    public String getBun() {
+        return bun;
+    }
+
+    public int getBurgers() {
+        return burgers;
+    }
+
+    public String getSauce() {
+        return sauce;
+    }
+
+    public Set<String> getIngredients() {
+        return ingredients;
+    }
+
+    @Override
+    public String toString() {
+        return "Bigmac{" +
+                "bun='" + bun + '\'' +
+                ", burgers=" + burgers +
+                ", sauce='" + sauce + '\'' +
+                ", ingredients=" + ingredients +
+                '}';
+    }
+}
---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -0,0 +1,38 @@
+package com.kodilla.patterns.builder.bigmac;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Set;
+
+public class BigmacTestSuite {
+
+    @Test
+    public void testBigMacBuilder() {
+
+        Bigmac bigmac = new Bigmac.BigmacBuilder()
+                .bun("Sesame bun")
+                .burgers(3)
+                .sauce("Standard")
+                .ingredient("Salad")
+                .ingredient("Onion")
+                .ingredient("Bacon")
+                .ingredient("Cheese")
+                .build();
+        System.out.println(bigmac);
+
+        String bun = bigmac.getBun();
+        int burgers = bigmac.getBurgers();
+        String sauce = bigmac.getSauce();
+        Set<String> ingredients = bigmac.getIngredients();
+
+        Assert.assertEquals("Sesame bun", bun);
+        Assert.assertEquals(3, burgers);
+        Assert.assertEquals("Standard", sauce);
+        Assert.assertTrue(ingredients.containsAll(Arrays.asList("Salad", "Cheese", "Onion", "Bacon")));
+        Assert.assertEquals(4, ingredients.size());
+    }
+
+}
